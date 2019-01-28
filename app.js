5859c9e320615544679000c4b006cb0d84c7d6cd
Tiko Mesropyan <tikodeveloper@gmail.com>
2019-01-28 15:57:23 +0400

    added about-us route.

--- app.js
+++ app.js
@@ -3,6 +3,20 @@
 const express = require('express');
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/hello/{name}', (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+});
+
+app.get('/about-us', (req, res) => {
+    res.send('We are developers!!!');
+});
+
 const server = app.listen(8888, () => {
     console.log('app is listening on port 8888 ...');
 });
